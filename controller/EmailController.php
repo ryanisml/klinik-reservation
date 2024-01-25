b661a5ff0ac8a2ab1067d89f55172640713fdcb1
Ryan Ismail <ryanisml@gmail.com>
2024-01-25 21:51:46 +0800

    Update .env.example and EmailController.php

--- controller/EmailController.php
+++ controller/EmailController.php
@@ -1,42 +1,39 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 class EmailController {
     public function send() {
-        ini_set( 'display_errors', 1 );
-        error_reporting( E_ALL );    
-        if ($_POST['emaildokter'] != null) {
-            $email = $_POST["emaildokter"];
-            $nama = $_POST["nama"];
-            $password = $_POST["password"];
-            $subject = "Kpd yth Dokter ".$nama." berikut ini akun untuk masuk aplikasi klinik";
-            $message = "
-            <html>
-            <head>
-            <title>Berikut Ini Kami Berikan Akun Dokter Anda</title>
-            </head>
-            <body>
-            <table>
-            <tr>
-            <th>Email</th>
-            <th>Kata Sandi</th>
-            </tr>
-            <tr>
-            <td>".$email."</td>
-            <td>".$password."</td>
-            </tr>
-            </table>
-            </body>
-            </html>";
-
-            // Always set content-type when sending HTML email
-            $headers = "MIME-Version: 1.0" . "\r\n";
-            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-            // More headers
-            $headers .= 'From: <ryan@ismail.id>';
-            if (!mail($email,$subject,$message,$headers)) {
-                $response['sukses'] = 'Gagal Kirim Password Ke Email';
-            }else{
+        $mail = new PHPMailer(true);
+        if (isset($_POST['emaildokter']) && isset($_POST['nama']) && isset($_POST['password'])) {
+            try {
+                $email = $_POST["emaildokter"];
+                $nama = $_POST["nama"];
+                $password = $_POST["password"];
+                //Server settings
+                $mail->SMTPDebug = SMTP::DEBUG_OFF;                         //Enable verbose debug output
+                $mail->isSMTP();                                            //Send using SMTP
+                $mail->Host       = 'smtp.hostinger.com';                   //Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                $mail->Username   = 'no-reply@ismail.id';                   //SMTP username
+                $mail->Password   = 'Testing123!!!';                        //SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+                $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            
+                //Recipients
+                $mail->setFrom('no-reply@ismail.id', '[No-Reply] Klinik Reservation');
+                $mail->addAddress($email, $nama);     //Add a recipient
+                
+                //Content
+                $mail->isHTML(true);                                  //Set email format to HTML
+                $mail->Subject = "[Registrasi Akun] Dokter ".$nama." berikut ini akun untuk masuk aplikasi klinik";
+                $mail->Body    = '<b>Berikut ini kami berikan akun dokter untuk anda gunakan masuk kedalam aplikasi</b><br/><table><tr><th>Email</th><th>Kata Sandi</th></tr><tr><td>'.$email.'</td><td>'.$password.'</td></tr></table>';
+            
+                $mail->send();
                 $response['sukses'] = 'Berhasil Kirim Password Ke Email';
+            } catch (Exception $e) {
+                $response['sukses'] = 'Gagal Kirim Password Ke Email';
             }
             echo json_encode($response);
         }else{
